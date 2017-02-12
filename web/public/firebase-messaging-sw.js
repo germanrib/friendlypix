8d91e97733d85fc8b32da6cd5a8875f8532e820c
Germans Ribakovs <Germans Ribakovs>
2017-02-12 15:02:00 +0200

    Added Firebase App data

--- web/public/firebase-messaging-sw.js
+++ web/public/firebase-messaging-sw.js
@@ -4,7 +4,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId: '670781077200'
+  messagingSenderId: '665144094157'
 });
 const messaging = firebase.messaging();
 
